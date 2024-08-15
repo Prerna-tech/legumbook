8cd3401bc3ff5805e30d815f80011c2432dee06f
Prerna Kulshrestha <67836322+Prerna-tech@users.noreply.github.com>
2024-08-15 20:51:58 +0530

    adding lang and views file

---
+++ resources/views/guest_question.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Guest Question</title>
+</head>
+<body>
+    <h4>i am {{$details->name}} .</h4>
+    <p>my question is {{$details->question}}</p>
+    <h5>my email id is {{$details->email}}</h5>
+</body>
+</html>
